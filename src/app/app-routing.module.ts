96023bdc996104ffaf02b95caa47d5714fd25f7a
BlakeBrewen <brewen.blake4@gmail.com>
2023-10-05 17:10:36 -0500

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,36 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RegistrationComponent } from './registration/registration.component';
+import { LoginComponent } from './login/login.component';
+import { ProfileComponent } from './profile/profile.component';
+import { FeedComponent } from './feed/feed.component';
+import { TweetComponent } from './tweet/tweet.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // Default route (redirect to the login page)
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+
+  // Registration page
+  { path: 'registration', component: RegistrationComponent },
+
+  // Login page
+  { path: 'login', component: LoginComponent },
+
+  // Profile page
+  { path: 'profile/:username', component: ProfileComponent },
+
+  // Feed page
+  { path: 'feed', component: FeedComponent },
+
+  // Tweet page (for creating a new tweet)
+  { path: 'tweet', component: TweetComponent },
+
+  // Add more routes for other features and pages as needed
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module'; 
 import { AppComponent } from './app.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { LoginComponent } from './login/login.component';
+import { ProfileComponent } from './profile/profile.component';
+import { FeedComponent } from './feed/feed.component';
+import { TweetComponent } from './tweet/tweet.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistrationComponent,
+    LoginComponent,
+    ProfileComponent,
+    FeedComponent,
+    TweetComponent
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
